6fc7ea6fc7bec40a7ecc449155bd673f3577b5d8
marjola3 <marjola3@poczta.onet.pl>
2014-05-12 19:18:30 +0200

    poprawki

---
+++ src/main/java/gui/accounts_table/view/AccountsFrame.java
@@ -0,0 +1,14 @@
+package gui.accounts_table.view;
+
+import org.springframework.stereotype.Component;
+
+import javax.swing.*;
+
+@Component()
+public class AccountsFrame extends JFrame {
+
+    public void init() {
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+    }
+}
---
+++ src/main/java/gui/accounts_table/view/AccountsPanel.java
@@ -0,0 +1,9 @@
+package gui.accounts_table.view;
+
+import javax.swing.*;
+
+/**
+ * Created by Mariola on 2014-05-12.
+ */
+public class AccountsPanel extends JPanel {
+}
