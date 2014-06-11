edf3a022771ca6c53e4a1156cb2f3e67a4eccb6e
marjola3 <marjola3@poczta.onet.pl>
2014-06-11 19:38:58 +0200

    poprawki

--- src/main/java/com/mp/email/dao/UserDao.java
+++ src/main/java/com/mp/email/dao/UserDao.java
@@ -8,11 +8,15 @@ import com.mp.email.xls.XlsUserProvider;
 import java.io.IOException;
 import java.util.List;
 
-public class UserDao {
+public class UserDao implements IUserDao {
     @Autowired
     private XlsUserProvider xlsUserProvider;
 
     public List<User> findAllUsers() throws IOException {
         return xlsUserProvider.getAllUsers();
+    }
+
+    public void saveAllUsers(List<User> users) {
+        xlsUserProvider.saveAllUser(users);
     }
 }
--- src/main/java/com/mp/email/gui/accounts_table/controller/AccountsController.java
+++ src/main/java/com/mp/email/gui/accounts_table/controller/AccountsController.java
@@ -1,19 +1,23 @@
 package com.mp.email.gui.accounts_table.controller;
 
+import com.mp.email.dao.IUserDao;
 import com.mp.email.dao.UserDao;
 import com.mp.email.gui.accounts_table.model.AccountsTableModel;
+import com.mp.email.gui.accounts_table.view.AccountsFrame;
 import com.mp.email.gui.accounts_table.view.ButtonPanel;
 import com.mp.email.gui.accounts_table.view.TablePanel;
 import com.mp.email.log.AutowiredLogger;
 import com.mp.email.model.User;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 
-import javax.annotation.PostConstruct;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.IOException;
 import java.util.List;
 
@@ -27,7 +31,7 @@ public class AccountsController {
     private Logger logger;
 
     @Autowired
-    private UserDao userDao;
+    private IUserDao userDao;
 
     @Autowired
     private AccountsTableModel accountsTableModel;
@@ -38,9 +42,22 @@ public class AccountsController {
     @Autowired
     private TablePanel tablePanel;
 
+    @Autowired
+    private AccountsFrame accountsFrame;
+
+    @Value("${accountsControler.removePaneMessage}")
+    private String removePaneMessage;
+
+    @Value("${accountsControler.removePaneTitle}")
+    private String remevePaneTitle;
+
     public void init(){
         findUsersAndFillTable();
+        initializeBtns();
+        accountsFrame.addWindowListener(new AccountsWindowListener());
+    }
 
+    private void initializeBtns() {
         JButton sendBtn = buttonPanel.getSendBtn();
         sendBtn.addActionListener(new SendBtnListener());
 
@@ -57,7 +74,7 @@ public class AccountsController {
             AccountsTableModel model = tablePanel.getAccountsTableModel();
             model.setUsers(allUsers);
         } catch (IOException e) {
-            logger.error("Błąd podczas pobierania użytkowników", e);
+            logger.error("Could not get users", e);
         }
     }
 
@@ -88,10 +105,34 @@ public class AccountsController {
             } else {
                 JOptionPane.showMessageDialog(
                         null,
-                        "Nie zaznaczono wiersza",
-                        "Info",
+                        removePaneMessage,
+                        remevePaneTitle,
                         JOptionPane.INFORMATION_MESSAGE);
             }
         }
+    }
+
+    class AccountsWindowListener extends WindowAdapter {
+        @Override
+        public void windowClosing(WindowEvent e) {
+            int result = JOptionPane.showConfirmDialog(
+                    null,
+                    "Czy chcesz?",
+                    "Informacja",
+                    JOptionPane.YES_NO_CANCEL_OPTION,
+                    JOptionPane.INFORMATION_MESSAGE);
+
+            if (result == JOptionPane.OK_OPTION) {
+                saveAllUsers();
+                System.exit(0);
+            } else if(result == JOptionPane.NO_OPTION) {
+                System.exit(0);
+            }
+        }
+    }
+
+    private void saveAllUsers() {
+        AccountsTableModel model = tablePanel.getAccountsTableModel();
+        userDao.saveAllUsers(model.getUsers());
     }
 }
